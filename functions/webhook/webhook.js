24d756ee184054b237780c1a600e2dbc73007436
Aaron <aloken128@gmail.com>
2019-10-14 21:50:45 -0600

    Webhook analytics

---
+++ functions/webhook/webhook.js
@@ -0,0 +1,33 @@
+const got = require('got')
+exports.handler = async function (event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
+  let data
+  try {
+    data = JSON.parse(event.body)
+  } catch (_) {
+    return { statusCode: 400, body: 'Badly Formatted Body' }
+  }
+
+  if (!data || !data.user) {
+    return { statusCode: 400, body: 'No content to send' }
+  }
+
+  try {
+    if (!process.env.WEBHOOK_URL) return { statusCode: 500, body: 'Missing env variable' }
+    const { displayName, provider, providerId } = data.user
+    await got.post(process.env.WEBHOOK_URL, {
+      body: {
+        content: `[Nightbot Playlist] - ${displayName} from ${provider} ${providerId}`,
+        username: 'Nightbot Playlist',
+        avatar_url: 'https://nightbot.tv/assets/images/login-logo.png'
+      },
+      json: true
+    })
+
+    return { statusCode: 204, body: '' }
+  } catch (error) {
+    return { statusCode: 500, body: 'Error sending message' }
+  }
+}
